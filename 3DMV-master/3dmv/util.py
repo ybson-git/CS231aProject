0a89b44c813b082e9434d56afb5a5fdf021fe555
Hill CW Chen <hill.magic@gmail.com>
2022-02-19 08:52:27 -0800

    1) Create Conda environment with necessary dependencies, 2) Cleaned up some Python2 syntax errors, 3) Format with code with Black.

--- 3DMV-master/3dmv/util.py
+++ 3DMV-master/3dmv/util.py
@@ -1,4 +1,3 @@
-
 import os, struct, math
 import numpy as np
 import torch
@@ -6,10 +5,10 @@ import torch
 # util for saving tensors, for debug purposes
 def write_array_to_file(tensor, filename):
     sz = tensor.shape
-    with open(filename, 'wb') as f:
-        f.write(struct.pack('Q', sz[0]))
-        f.write(struct.pack('Q', sz[1]))
-        f.write(struct.pack('Q', sz[2]))
+    with open(filename, "wb") as f:
+        f.write(struct.pack("Q", sz[0]))
+        f.write(struct.pack("Q", sz[1]))
+        f.write(struct.pack("Q", sz[2]))
         tensor.tofile(f)
 
 
@@ -33,23 +32,33 @@ def make_intrinsic(fx, fy, mx, my):
 def adjust_intrinsic(intrinsic, intrinsic_image_dim, image_dim):
     if intrinsic_image_dim == image_dim:
         return intrinsic
-    resize_width = int(math.floor(image_dim[1] * float(intrinsic_image_dim[0]) / float(intrinsic_image_dim[1])))
-    intrinsic[0,0] *= float(resize_width)/float(intrinsic_image_dim[0])
-    intrinsic[1,1] *= float(image_dim[1])/float(intrinsic_image_dim[1])
+    resize_width = int(
+        math.floor(
+            image_dim[1] * float(intrinsic_image_dim[0]) / float(intrinsic_image_dim[1])
+        )
+    )
+    intrinsic[0, 0] *= float(resize_width) / float(intrinsic_image_dim[0])
+    intrinsic[1, 1] *= float(image_dim[1]) / float(intrinsic_image_dim[1])
     # account for cropping here
-    intrinsic[0,2] *= float(image_dim[0]-1)/float(intrinsic_image_dim[0]-1)
-    intrinsic[1,2] *= float(image_dim[1]-1)/float(intrinsic_image_dim[1]-1)
+    intrinsic[0, 2] *= float(image_dim[0] - 1) / float(intrinsic_image_dim[0] - 1)
+    intrinsic[1, 2] *= float(image_dim[1] - 1) / float(intrinsic_image_dim[1] - 1)
     return intrinsic
 
 
 def get_sample_files(samples_path):
-    files = [f for f in os.listdir(samples_path) if f.endswith('.sample')] #and os.path.isfile(join(samples_path, f))]
+    files = [
+        f for f in os.listdir(samples_path) if f.endswith(".sample")
+    ]  # and os.path.isfile(join(samples_path, f))]
     return files
 
 
 def get_sample_files_for_scene(scene, samples_path):
-    files = [f for f in os.listdir(samples_path) if f.startswith(scene) and f.endswith('.sample')] #and os.path.isfile(join(samples_path, f))]
-    print 'found ', len(files), ' for ', os.path.join(samples_path, scene)
+    files = [
+        f
+        for f in os.listdir(samples_path)
+        if f.startswith(scene) and f.endswith(".sample")
+    ]  # and os.path.isfile(join(samples_path, f))]
+    print("found ", len(files), " for ", os.path.join(samples_path, scene))
     return files
 
 
@@ -58,7 +67,7 @@ def load_pose(filename):
     pose = torch.Tensor(4, 4)
     lines = open(filename).read().splitlines()
     assert len(lines) == 4
-    lines = [[x[0],x[1],x[2],x[3]] for x in (x.split(" ") for x in lines)]
+    lines = [[x[0], x[1], x[2], x[3]] for x in (x.split(" ") for x in lines)]
     return torch.from_numpy(np.asarray(lines).astype(np.float32))
 
 
@@ -67,7 +76,7 @@ def read_class_weights_from_file(filename, num_classes, normalize):
     weights = torch.zeros(num_classes)
     lines = open(filename).read().splitlines()
     for line in lines:
-        parts = line.split('\t')
+        parts = line.split("\t")
         assert len(parts) == 2
         weights[int(parts[0])] = int(parts[1])
     if normalize:
